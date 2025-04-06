095998b9e40d7dfd4790996bd6cf908b6db02150
Diler <d1l3r@hotmail.com>
2025-04-06 11:25:17 +0100

    terraform state

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraform_statefile" {
-  bucket = "terraform_statefile"
+resource "aws_s3_bucket" "terraform-statefile-diler" {
+  bucket = "terraform-statefile-diler"
+}
+terraform {
+  backend "s3" {
+    bucket         = "terraform-statefile-diler"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
 }
